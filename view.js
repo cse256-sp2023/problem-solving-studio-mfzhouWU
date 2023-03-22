069077ced60b3dcab095e47f560112133331c5df
mfzhouWU <mfzhou@wustl.edu>
2023-03-21 22:17:03 -0500

    ps studio complete

--- view.js
+++ view.js
@@ -1,6 +1,49 @@
 // ---- Define your dialogs  and panels here ----
+let panel = define_new_effective_permissions("panel_id", add_info_col = true)
+let user_sel = define_new_user_select_field("user_id", "Select User", on_user_change = function(selected_user)
+{
+    $('#panel_id').attr('filepath', '/C/presentation_documents/important_file.txt')
+    $('#panel_id').attr('username', selected_user)
+})
 
+$('#sidepanel').append(user_sel)
+$('#sidepanel').append(panel)
 
+// Question: What is the code necessary to build and display the effective permissions panel?
+     // Need the function, this is what builds the panel, and append is what displays it
+// Question: Which permissions does the administrator have for /C?
+    // The administrator has all/full permissions for /C
+// Question: What does the html look like for your effective permissions panel now?
+    // Some permissions are now checked off (the permissions have changed)
+// Question: What is your js code to create and add the user selection button/dialog?
+    // See above (but without the jquery additions)
+// Question: What does the completed line to create the user select field look like?
+    // See above, but with jquery
+
+let dialog = define_new_dialog("dialog_id", title='Permission Info')
+$('.perm_info').click(function(){
+    // 1
+    $('#dialog_id').dialog('open')
+    // 2
+    // console.log($('#panel_id').attr('filepath'))
+    // console.log($('#panel_id').attr('username'))
+    // console.log($(this).attr('permission_name'))
+
+    file_obj = path_to_file[$('#panel_id').attr('filepath')]
+    user_obj = all_users[$('#panel_id').attr('username')]
+    perm_name = $(this).attr('permission_name')
+
+    let explanation_obj = allow_user_action(file_obj, user_obj, perm_name, explain_why = true)
+    let explanation_text = get_explanation_text(explanation_obj)
+    $('#dialog_id').text(explanation_text)
+})
+
+// Question: What does your initial click listener look like?
+    // See above 1
+// Question: What does your full console log method look like (please include its class selector)?
+    // See above 2
+// Question: What does your final click handler look like?
+    // See above (current)
 
 // ---- Display file structure ----
 
